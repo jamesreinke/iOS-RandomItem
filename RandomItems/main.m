05a92e711ff1ea3833d89ccc571bf338a0d05cbe
jamesreinke <james@rtsystems.io>
2015-07-17 13:11:22 -0700

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by James Reinke on 7/17/15.
+//  Copyright (c) 2015 Penguin Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
